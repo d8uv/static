0ecf288dea02fe711c3206d4c2d04f8a19a90796
d8uv <d8uv@d8uv.org>
2010-02-20 11:13:02 -0900

    With animation. Is awesome. Now is the time for testing in other browsers & refactoring.

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ eqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true,
 newcap: true, immed: true */
 "use strict";
 onload = function () {
-    var hex, out, i;
+    var hex, out;
 
     hex = '0,1,2,3,4,5,6,7,8,9,0,a,b,c,d,e,f'.split(',');
     out = [];
 
-    for (i = 0; i < 5000; i = i + 1) {
-	out.unshift(hex[Math.floor(Math.random() * hex.length)]);
-	document.getElementsByTagName('p')[0].innerText = out.join(' ');
-    }
+    setInterval(
+	function () {
+	    out.unshift(hex[Math.floor(Math.random() * hex.length)]);
+	    document.getElementsByTagName('p')[0].innerText = out.join(' ');
+
+	    if (out.length > 5000) {
+		out.pop();
+	    }
+	}, 100
+    );
 };
