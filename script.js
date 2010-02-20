8ee644f1eef6b51e6a70394d1e5144dcdcecda8a
d8uv <d8uv@d8uv.org>
2010-02-20 14:31:17 -0900

    jqueryized

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-/*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, 
-eqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, 
-newcap: true, immed: true */
-
 "use strict";
 
-onload = function () {
+$(document).ready(function () {
     var max = 7500;
     var out = [];
 
@@ -12,11 +8,11 @@ onload = function () {
         var hex = '01234567890abcdef';
 
         out.unshift(hex[Math.floor(Math.random() * hex.length)]);
-        document.getElementsByTagName('p')[0].innerHTML = out.join(' ');
+        $('p').text(out.join(' '));
 
         if (out.length > max) out.pop();
     }
 
     setInterval(scrollHex, 100);
     for (var i = 0; i < max; i = i + 1) scrollHex();
-};
+});
