54fee870608221a1cd8e58805d816f19843c3386
Sean B. Palmer <sean@miscoranda.com>
2010-02-20 15:36:22 -0700

    Did something with the something to achieve something indistinct.

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, 
 eqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, 
 newcap: true, immed: true */
-"use strict";
-onload = function () {
-    var i, out, max;
 
-    max = 7500;
-    out = [];
+"use strict";
+
+onload = function () {
+    var max = 7500;
+    var out = [];
 
     function scrollHex() {
-        var hex = '0,1,2,3,4,5,6,7,8,9,0,a,b,c,d,e,f'.split(',');
+        var hex = '01234567890abcdef';
 
         out.unshift(hex[Math.floor(Math.random() * hex.length)]);
         document.getElementsByTagName('p')[0].innerHTML = out.join(' ');
 
-	if (out.length > max) {
-            out.pop();
-        }
+        if (out.length > max) out.pop();
     }
 
     setInterval(scrollHex, 100);
-    for (i = 0; i < max; i = i + 1) {
-        scrollHex();
-    }
+    for (var i = 0; i < max; i = i + 1) scrollHex();
 };
