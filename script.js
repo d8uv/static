71495aca5c1cfcb668446e36372a33731757e89f
d8uv <d8uv@d8uv.org>
2010-02-20 09:35:06 -0900

    dummy hex in html, style's pretty much done, scripting needs work

---
+++ script.js
@@ -0,0 +1,5 @@
+/*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, 
+eqeq: true, plusplus: true, bitwise: true, regexp: true, strict: true, 
+newcap: true, immed: true */
+onload = function() {
+};
